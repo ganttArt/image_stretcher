gradient_length and image_width variables added, gradient algorithm fixed

gradient_tests.py
@@ -8,28 +8,30 @@ FILENAME = '2h10w.jpg'
 gradient_length = 10
 
 img = Image.open(FILENAME, 'r')
-print(img)
+image_width = img.size[0]
+
 
 np_array = np.array(img)
 
 # new_image = np.zeros((10, 10, 3), dtype=np.uint8)
-new_image = np.zeros((gradient_length, 10, 3))
+new_image = np.zeros((gradient_length+2, image_width, 3))
 
 new_image[0] += np_array[0]
 new_image[-1] += np_array[-1]
 
 diff_array = abs(new_image[0]-new_image[-1])
 
-for num in range(1, 9):
-    for row in range(10):
+# for num in range(1, 9):
+for num in range(gradient_length):
+    for row in range(image_width):
         # print(row)
         for column in range(3):
             if new_image[0][row][column] >= new_image[-1][row][column]:
-                new_image[num][row][column] += (new_image[0][row][column] - ((diff_array[row][column])/10*num))                
+                new_image[num+1][row][column] += (new_image[0][row][column] - ((diff_array[row][column])/(gradient_length+1)*(num+1)))                
             else:
-                new_image[num][row][column] += (new_image[0][row][column] + ((diff_array[row][column])/10*num))
+                new_image[num+1][row][column] += (new_image[0][row][column] + ((diff_array[row][column])/(gradient_length+1)*(num+1)))
 
-# print(new_image)
+print(new_image)
 
 #convert np array to integers
 even_newer = new_image.astype(np.uint8)