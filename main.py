393e7d454e424f6c4a12dba11b8e40c6c3684c7d
ganttArt <gantt.art@gmail.com>
2019-12-13 11:48:24 -0800

    horizontal stretch slider added and starting_point_slider renamed to vertical_slider

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ class Gui:
     def __init__(self, master):
 
         self.image_frame = ttk.Frame(master)
-        self.image_frame.pack(side='left', anchor='nw', padx=10, pady=10)
+        self.image_frame.pack(side='left', anchor='nw', padx=8, pady=8)
 
         self.art = PhotoImage(file='intro_image.gif')
         self.art_label = ttk.Label(self.image_frame, image=self.art)
         self.art_label.grid(row=0, column=0)
 
+        self.horizontal_slider = ttk.Scale(self.image_frame, orient='horizontal',
+                                           from_=1, to=int(self.art.width()),
+                                           length=int(self.art.width()))
+        self.horizontal_slider.grid(row=1, column=0)
+
+        self.vertical_slider = ttk.Scale(self.image_frame, orient='vertical',
+                                         from_=1, to=int(self.art.height()),
+                                         length=int(self.art.height()))
+        self.vertical_slider.grid(row=0, column=1)
+
         self.widget_frame = ttk.Frame(master)
-        self.widget_frame.pack(side='right', anchor='ne', padx=10, pady=10)
+        self.widget_frame.pack(anchor='nw', padx=8, pady=8)
 
         self.open_button = ttk.Button(self.widget_frame, text="Open", command=self.open_image)
         self.open_button.grid(row=0, column=1)
@@ -32,30 +42,14 @@ class Gui:
         self.intensity_value = StringVar()
         self.intensity_value.set("13")
         self.intensity_spinbox = ttk.Spinbox(self.widget_frame, from_=1, to=13, width=5,
-                                             textvariable=self.intensity_value,
-                                             # command=self.stretch_image
-                                             )
+                                             textvariable=self.intensity_value,)
         self.intensity_spinbox.grid(row=1, column=2, pady=0)
 
-
-        # ttk.Label(self.widget_frame, text='           Rate').grid(row=2, column=1)
-        # self.rate_value = StringVar()
-        # self.rate_value.set("1")
-        # self.rate_spinbox = ttk.Spinbox(self.widget_frame, from_=1, to=50, width=5,
-        #                                 textvariable=self.rate_value,
-        #                                 # command=self.stretch_image
-        #                                 )
-        # self.rate_spinbox.grid(row=2, column=2)
 
         self.random_button = ttk.Button(self.widget_frame, text='Random',
                                         command=self.random_stretch)
         self.random_button.grid(row=2, column=1, columnspan=2)
 
-
-        self.starting_point_slider = ttk.Scale(self.widget_frame, orient='vertical',
-                                               from_=1, to=int(self.art.height()),
-                                               length=int(self.art.height()))
-        self.starting_point_slider.grid(row=0, column=0, rowspan=100)
 
         self.animate_button = ttk.Button(self.widget_frame, text="Animate",
                                          command=self.downward_stretch
@@ -73,12 +67,11 @@ class Gui:
                                            # int(self.rate_value.get())
                                            )
             self.processed_image = build_new_image(index_list, img_array,
-                                                   self.starting_point_slider.get())
+                                                   self.vertical_slider.get())
         except AttributeError:
             # exception handling for trying to change variables before opening initial image
             messagebox.showerror("Error", "Please open an image file before stretching")
             self.intensity_value.set("10")
-            # self.rate_value.set("1")
 
     def display_image(self):
         self.art = ImageTk.PhotoImage(self.processed_image)
@@ -102,11 +95,17 @@ class Gui:
 
         self.unprocessed_jpg = jpg_image
 
-        self.starting_point_slider = ttk.Scale(self.widget_frame, orient='vertical',
+        self.vertical_slider = ttk.Scale(self.image_frame, orient='vertical',
                                                from_=1, to=self.unprocessed_jpg.size[1],
                                                length=self.unprocessed_jpg.size[1])
-        self.starting_point_slider.grid(row=0, column=0, rowspan=100)
-        self.starting_point_slider.set(100)
+        self.vertical_slider.grid(row=0, column=1)
+        self.vertical_slider.set(100)
+
+        self.horizontal_slider = ttk.Scale(self.image_frame, orient='horizontal',
+                                           from_=1, to=self.unprocessed_jpg.size[0],
+                                           length=self.unprocessed_jpg.size[0])
+        self.horizontal_slider.grid(row=1, column=0)
+        self.horizontal_slider.set(100)
 
         self.stretch_image()
         self.display_image()
@@ -116,7 +115,7 @@ class Gui:
         self.processed_image.save(save_filename)
 
     def random_stretch(self):
-        self.starting_point_slider.set(random.randint(1, self.unprocessed_jpg.size[1]))
+        self.vertical_slider.set(random.randint(1, self.unprocessed_jpg.size[1]))
         self.intensity_value.set(random.randint(1,13))
         self.stretch_image()
         self.display_image()
