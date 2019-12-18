e058ef10168571c40932f4e658ebd8396f599812
ganttArt <gantt.art@gmail.com>
2019-12-18 06:23:24 -0800

    Pylint run on main.py and stretching_functions.py; changes made to account for an unnecessary parameter in create_index_list function, ie. passing in the np_array

--- main.py
+++ main.py
@@ -96,14 +96,14 @@ class Gui:
 
     def downward_stretch(self):
         img_array = create_np_array(self.unprocessed_jpg)
-        index_list = create_index_list(img_array, int(self.intensity_value.get()))
+        index_list = create_index_list(int(self.intensity_value.get()))
         self.processed_image = build_new_image(index_list, img_array, self.vertical_slider.get())
         self.display_image()
 
     def upward_stretch(self):
         self.unprocessed_jpg = self.unprocessed_jpg.rotate(180)
         img_array = create_np_array(self.unprocessed_jpg)
-        index_list = create_index_list(img_array, int(self.intensity_value.get()))
+        index_list = create_index_list(int(self.intensity_value.get()))
         starting_pixel = self.vertical_index_list[int(self.vertical_slider.get())* -1]
         self.processed_image = build_new_image(index_list, img_array, starting_pixel)
         self.processed_image = self.processed_image.rotate(180)
@@ -113,7 +113,7 @@ class Gui:
     def right_stretch(self):
         self.unprocessed_jpg = self.unprocessed_jpg.rotate(270, expand=True)
         img_array = create_np_array(self.unprocessed_jpg)
-        index_list = create_index_list(img_array, int(self.intensity_value.get()))
+        index_list = create_index_list(int(self.intensity_value.get()))
         self.processed_image = build_new_image(index_list, img_array, self.horizontal_slider.get())
         self.processed_image = self.processed_image.rotate(90, expand=True)
         self.display_image()
@@ -123,7 +123,7 @@ class Gui:
         self.unprocessed_jpg = self.unprocessed_jpg.rotate(90, expand=True)
         # self.unprocessed_jpg.show()
         img_array = create_np_array(self.unprocessed_jpg)
-        index_list = create_index_list(img_array, int(self.intensity_value.get()))
+        index_list = create_index_list(int(self.intensity_value.get()))
         starting_pixel = self.horizontal_index_list[int(self.horizontal_slider.get()) * -1]
         self.processed_image = build_new_image(index_list, img_array, starting_pixel)
         self.processed_image = self.processed_image.rotate(270, expand=True)
--- stretching_functions.py
+++ stretching_functions.py
@@ -1,21 +1,23 @@
+'''Stretching functions for image stretcher, accessed through main.py'''
+
 from PIL import Image
 import numpy as np
-
 
 def create_np_array(pil_image):
     '''converts a jpg to a numpy array'''
     return np.array(pil_image)
 
 
-def create_index_list(np_array, multiplication_factor=13):
+def create_index_list(multiplication_factor=13):
     '''
     Returns:
         list
     '''
-    inverse_value_dict = {13:1, 12:1.25, 11:1.5, 10:1.75, 9:2, 8:2.25, 7:2.5, 6:2.75, 5:3, 4:3.25, 3:3.5, 2:3.75, 1:4}
+    inverse_value_dict = {13:1, 12:1.25, 11:1.5, 10:1.75, 9:2, 8:2.25, 7:2.5, 6:2.75, 5:3, 4:3.25,
+                          3:3.5, 2:3.75, 1:4}
 
-    pairs = [[1,13], [2,8], [3,5], [5,3], [8,2], [13,1], [21,1], [34,1], [55,1], [89,1], [144,1],
-             [233,1], [377,1], [610, 1], [987, 1]]
+    pairs = [[1, 13], [2, 8], [3, 5], [5, 3], [8, 2], [13, 1], [21, 1], [34, 1], [55, 1], [89, 1],
+             [144, 1], [233, 1], [377, 1], [610, 1], [987, 1]]
 
     for pair in pairs:
         pair[0] = pair[0] * 1
@@ -30,6 +32,7 @@ def create_index_list(np_array, multiplication_factor=13):
 
 
 def create_gradient(two_row_array, gradient_size):
+    '''returns a gradient made from two rows of pixels'''
     image_width = two_row_array.shape[1]
     gradient_array = np.zeros((gradient_size+2, image_width, 3))
     gradient_array[0] += two_row_array[0]
@@ -54,7 +57,7 @@ def build_new_image(index_list, source_image, starting_pixel):
     starting_pixel = int(starting_pixel)
     index_value = starting_pixel
     index_list_value = 0
-    
+
     new_image = np.zeros((source_image.shape))
 
     for num in range(starting_pixel):
@@ -67,16 +70,16 @@ def build_new_image(index_list, source_image, starting_pixel):
         two_row_array = np.zeros((2, width, 3))
         two_row_array[0] += source_image[num]
         two_row_array[1] += source_image[num+1]
-        
+
         gradient_array = create_gradient(two_row_array, index_list[index_list_value])
         index_list_value += 1
-        
+
         try:
             for number in range(gradient_array.shape[0] - 1):
             # for number in range(gradient_array.shape[0] - 1):
                 new_image[index_value] += gradient_array[number]
                 index_value += 1
-        except IndexError as e:
+        except IndexError:
             break
 
     int_array = new_image.astype(np.uint8)
