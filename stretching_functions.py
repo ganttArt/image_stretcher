471a5b0860651dece8918fbb34dc382bfc85c5d6
Christopher Gantt <gantt.art@gmail.com>
2023-01-08 13:24:52 -1000

    Linter on stretching_functions

--- stretching_functions.py
+++ stretching_functions.py
@@ -3,6 +3,7 @@
 from PIL import Image
 import numpy as np
 
+
 def create_np_array(pil_image):
     '''converts a jpg to a numpy array'''
     return np.array(pil_image)
@@ -13,8 +14,8 @@ def create_index_list(multiplication_factor=13):
     Returns:
         list
     '''
-    inverse_value_dict = {13:1, 12:1.25, 11:1.5, 10:1.75, 9:2, 8:2.25, 7:2.5, 6:2.75, 5:3, 4:3.25,
-                          3:3.5, 2:3.75, 1:4}
+    inverse_value_dict = {13: 1, 12: 1.25, 11: 1.5, 10: 1.75, 9: 2, 8: 2.25, 7: 2.5, 6: 2.75, 5: 3, 4: 3.25,
+                          3: 3.5, 2: 3.75, 1: 4}
 
     pairs = [[1, 13], [2, 8], [3, 5], [5, 3], [8, 2], [13, 1], [21, 1], [34, 1], [55, 1], [89, 1],
              [144, 1], [233, 1], [377, 1], [610, 1], [987, 1]]
@@ -43,9 +44,11 @@ def create_gradient(two_row_array, gradient_size):
         for row in range(image_width):
             for column in range(3):
                 if gradient_array[0][row][column] >= gradient_array[-1][row][column]:
-                    gradient_array[num+1][row][column] += (gradient_array[0][row][column] - ((difference_array[row][column])/(gradient_size+1)*(num+1)))
+                    gradient_array[num+1][row][column] += (gradient_array[0][row][column] - (
+                        (difference_array[row][column])/(gradient_size+1)*(num+1)))
                 else:
-                    gradient_array[num+1][row][column] += (gradient_array[0][row][column] + ((difference_array[row][column])/(gradient_size+1)*(num+1)))
+                    gradient_array[num+1][row][column] += (gradient_array[0][row][column] + (
+                        (difference_array[row][column])/(gradient_size+1)*(num+1)))
     return gradient_array
 
 
@@ -71,12 +74,13 @@ def build_new_image(index_list, source_image, starting_pixel):
         two_row_array[0] += source_image[num]
         two_row_array[1] += source_image[num+1]
 
-        gradient_array = create_gradient(two_row_array, index_list[index_list_value])
+        gradient_array = create_gradient(
+            two_row_array, index_list[index_list_value])
         index_list_value += 1
 
         try:
             for number in range(gradient_array.shape[0] - 1):
-            # for number in range(gradient_array.shape[0] - 1):
+                # for number in range(gradient_array.shape[0] - 1):
                 new_image[index_value] += gradient_array[number]
                 index_value += 1
         except IndexError:
